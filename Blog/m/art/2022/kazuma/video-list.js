4a4cfcfbb12fb140a368539b53c9341421ab26eb
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-05-27 16:51:40 +0700

    first commit

--- Blog/m/art/2022/kazuma/video-list.js
+++ Blog/m/art/2022/kazuma/video-list.js
@@ -1,7 +1,22 @@
 let allVideos = [
    {
-      name: "Kazuma Art Part 1",
-      src: "kazuma",
+      name: "Kazuma ( Original )",
+      src: "kazuma-ori",
       id: "vid_1"
+   },
+   {
+      name: "Kazuma ( 60 Detik )",
+      src: "kazuma-60d",
+      id: "vid_2"
+   },
+   {
+      name: "Kazuma ( 30 Detik )",
+      src: "kazuma-30d",
+      id: "vid_3"
+   },
+   {
+      name: "Kazuma (15 Detik )",
+      src: "kazuma-15d",
+      id: "vid_4"
    }
 ]
