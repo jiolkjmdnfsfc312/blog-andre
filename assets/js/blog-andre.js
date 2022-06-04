afcd6130f51cce669913dad06e72f7e318f8fa82
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-06-04 09:25:38 +0700

    first commit

--- assets/js/blog-andre.js
+++ assets/js/blog-andre.js
@@ -1,4 +1,4 @@
-/* Splash Screen */
+/*=============== Splash Screen ===============*/
 var loader = document.querySelector(".loader")
 
 window.addEventListener("load", vanish);
@@ -6,3 +6,32 @@ window.addEventListener("load", vanish);
 function vanish() {
   loader.classList.add("disppear");
 }
+
+/*=============== PWA Download ===============*/
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
